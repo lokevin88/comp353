9b49c4b87a8c0cba7f67d024dd205064104f956c
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-11-22 00:55:18 -0500

    hold 6

--- src/shared/commonGroupAndEvent.php
+++ src/shared/commonGroupAndEvent.php
@@ -8,7 +8,8 @@
                 <p>placeholder3EventName</p>
                 <div class="btn-group groupAndEventGroup">
                     <button type="button" class="btn bg-dark text-white">View More</button>
-                    <button type="button" class="btn bg-dark text-white">Create Event</button>
+                    <button type="button" class="btn bg-dark text-white" data-toggle="modal"
+                        data-target="#createEvent">Create Event</button>
                 </div>
             </div>
         </div>
@@ -24,6 +25,29 @@
                     <button type="button" class="btn bg-dark text-white">View More</button>
                     <button type="button" class="btn bg-dark text-white">Create Group</button>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="createEvent" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- logic tmr -->
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
