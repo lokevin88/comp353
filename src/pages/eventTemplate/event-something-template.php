856a9e75453bc28414266c0b18399eebd5da8862
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-11-22 21:54:14 -0500

    event logic

---
+++ src/pages/eventTemplate/event-something-template.php
@@ -0,0 +1,30 @@
+<?php
+    require $_SERVER['DOCUMENT_ROOT'] . '/comp353/src/shared/navbar.php';
+    //  always import from below here 
+  ?>
+
+<div class="main-body">
+
+  <?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/comp353/src/components/bannerAndMessage-component.php';
+  ?>
+
+  <div class="row">
+    <div class="col-lg-9">
+      <!-- put things here -->
+
+    </div>
+    <div class="col-lg-3">
+      <?php
+        include $_SERVER['DOCUMENT_ROOT'] . '/comp353/src/components/commonGroupAndEvent-component.php';
+      ?>
+    </div>
+  </div>
+</div>
+
+<?php
+
+    //  always import from above here  
+    require $_SERVER['DOCUMENT_ROOT'] . '/comp353/src/shared/jsScript.php';
+  ?>
+  
