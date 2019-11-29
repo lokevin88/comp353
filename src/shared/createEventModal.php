e62abae8195fd2a7f49d54627e2c56a097f445a4
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-11-28 20:38:12 -0500

    hold1

--- src/shared/createEventModal.php
+++ src/shared/createEventModal.php
@@ -13,17 +13,17 @@
                     <div class="form-group">
                         <label for="eventName">Event</label>
                         <input type="text" class="form-control" name="eventName" id="eventName"
-                            placeholder="Enter event name" pattern="[^\s]+" title="Input text" required>
+                            placeholder="Enter event name" pattern="[^\s]+" title="No blanks" required>
                     </div>
                     <div class="form-group">
                         <label for="eventDescription">Description</label>
                         <input type="text" class="form-control" name="eventDescription" id="eventDescription"
-                            placeholder="Enter description" pattern="[^\s]+" title="Input text" required>
+                            placeholder="Enter description" pattern="[^\s]+" title="No blanks" required>
                     </div>
                     <div class="form-group">
                         <label for="eventPhoneNumber">Phone number</label>
                         <input type="text" class="form-control" name="eventPhoneNumber" id="eventPhoneNumber"
-                            placeholder="Enter phone number" pattern="[^\s]+" title="Input text" required>
+                            placeholder="Enter phone number" pattern="[^\s]+" title="No blanks" required>
                     </div>
                     <fieldset class="form-group">
                         <p class="col-form-label pt-0">Event type</p>
