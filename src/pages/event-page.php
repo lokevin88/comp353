c4790c8602e1564b77e1b75fe0733f6b7ae64fcf
bgambocjaviniar <brian_javiniar@hotmail.com>
2019-12-05 03:41:58 -0500

    test

--- src/pages/event-page.php
+++ src/pages/event-page.php
@@ -118,16 +118,12 @@
             array_push($errors1, $billingAddressLength);
         }
 
-        if(empty($errors1)) {
-            echo 'good';
-            $array = array($cardNumber, $cardHolderName, $securityCode, $billingAddress);
-            $debitDetailsID = $user->insertPaymentInfo($array);
-            $user->updateEventManagerDebitDetails($debitDetailsID);
-            navigateTo("https://rrc353.encs.concordia.ca/comp353/src/pages/event-page.php");
-        }
-        else {
-            echo 'bad';
-        }
+
+        $array = array($cardNumber, $cardHolderName, $securityCode, $billingAddress);
+        $debitDetailsID = $user->insertPaymentInfo($array);
+        $user->updateEventManagerDebitDetails($debitDetailsID);
+        navigateTo("https://rrc353.encs.concordia.ca/comp353/src/pages/event-page.php");
+
     }
 
     if(isset($_POST['paying'])) {
